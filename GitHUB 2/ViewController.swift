538bb2c0eb9f99187589e95c791aeeba155b1527
adwik <adwiksajeev@gmail.com>
2023-06-18 14:38:44 +0530

    Initial Commit

---
+++ GitHUB 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHUB 2
+//
+//  Created by Adwaith V V on 18/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHUB 2UITests/GitHUB_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHUB_2UITestsLaunchTests.swift
+//  GitHUB 2UITests
+//
+//  Created by Adwaith V V on 18/06/23.
+//
+
+import XCTest
+
+final class GitHUB_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
